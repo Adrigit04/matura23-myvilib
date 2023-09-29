fece9e06e62f0f3517663ea1bc9e096de2256793
Adriano Carlucci <adrianovkvk@gmail.com>
2023-09-29 19:19:52 +0200

    mit vilib Ordner ergänzt

---
+++ vilib/matura23.py
@@ -0,0 +1,7 @@
+# matura23 some vars and util functions
+class Matura23Utils(object):
+
+    @staticmethod
+    def isMatura23Model(model_path=""):
+        return "matura23" in model_path.lower()
+
